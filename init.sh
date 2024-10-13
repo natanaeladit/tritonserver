1b43eb09f9aa9eb98f06d24b1db202e589cb4b18
natanaeladit <nata.adit@gmail.com>
2024-10-13 15:48:18 +0800

    working

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 # Download model
 mkdir -p ./models/densenet_onnx/1;
-wget -O models/densenet_onnx/1/model.onnx https://github.com/onnx/models/blob/main/validated/vision/classification/densenet-121/model/densenet-12.onnx;
+wget -O "models/densenet_onnx/1/model.onnx" "https://media.githubusercontent.com/media/onnx/models/refs/heads/main/validated/vision/classification/densenet-121/model/densenet-12.onnx?download=true";
 
 # Download client code
-wget -O client.py https://raw.githubusercontent.com/triton-inference-server/tutorials/main/Quick_Deploy/ONNX/client.py;
+wget -O client.py "https://raw.githubusercontent.com/triton-inference-server/tutorials/main/Quick_Deploy/ONNX/client.py";
 
 # Download image to inference
 wget -O img1.jpg "https://www.hakaimagazine.com/wp-content/uploads/header-gulf-birds.jpg";
